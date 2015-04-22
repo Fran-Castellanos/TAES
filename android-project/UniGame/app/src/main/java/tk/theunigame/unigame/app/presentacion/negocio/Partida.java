d7c3d494cb19c237a0a575225959c23335a0b7d7
Fran-Castellanos <fjcr88@gmail.com>
2015-04-22 13:55:34 +0200

    Clase Partida

---
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/presentacion/negocio/Partida.java
@@ -0,0 +1,29 @@
+package tk.theunigame.unigame.app.presentacion.negocio;
+
+/**
+ * Created by Paco on 22/04/2015.
+ */
+public class Partida {
+
+    private int numPreguntas;
+
+    public Partida()
+    {
+        numPreguntas = 20;
+
+    }
+
+
+    public Partida(int n)
+    {
+        numPreguntas = n;
+    }
+
+
+    public void jugar()
+    {
+
+
+    }
+
+}
