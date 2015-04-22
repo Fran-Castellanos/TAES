0d4f22649b3e25f8e61d51ffb9ce30464565a16d
Javier Ortega <fantasma345@hotmail.es>
2015-04-22 17:24:26 +0200

    Cambios en Fachadas pregunta y respuesta y crearpregunta

--- android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaPregunta.java
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaPregunta.java
@@ -1,7 +1,19 @@
 package tk.theunigame.unigame.app.fachadas;
 
+import java.util.ArrayList;
+
 /**
  * Created by Pedro on 21/04/2015.
  */
 public class FachadaPregunta {
+    private String contenido;
+    private ArrayList<FachadaRespuesta> respuestas;
+
+    public FachadaPregunta(String str, FachadaRespuesta a, FachadaRespuesta b, FachadaRespuesta c,FachadaRespuesta d){
+        contenido=str;
+        //queda hacer las comprobaciones
+        respuestas.add(a); respuestas.add(b); respuestas.add(c); respuestas.add(d);
+
+    }
+
 }
--- android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaRespuesta.java
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaRespuesta.java
@@ -4,4 +4,17 @@ package tk.theunigame.unigame.app.fachadas;
  * Created by Pedro on 21/04/2015.
  */
 public class FachadaRespuesta {
+
+    private String contenido;
+    private boolean correcto;
+
+
+    public FachadaRespuesta(String str){
+        contenido=str;
+        correcto=false;
+    }
+
+    public void setCorrecto(boolean nuevo){correcto=nuevo;}
 }
+
+
