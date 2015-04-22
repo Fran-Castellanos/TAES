cfab2e59e3bc9082bdb39d56ff1501440c1761aa
Javier Ortega <fantasma345@hotmail.es>
2015-04-22 19:26:09 +0200

    Mas cambios controlador

--- android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaPregunta.java
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/fachadas/FachadaPregunta.java
@@ -2,6 +2,9 @@ package tk.theunigame.unigame.app.fachadas;
 
 import java.util.ArrayList;
 
+import juego.taes.domainmodel.Model.Cliente.Pregunta;
+import juego.taes.domainmodel.Model.Cliente.Respuesta;
+
 /**
  * Created by Pedro on 21/04/2015.
  */
@@ -10,10 +13,13 @@ public class FachadaPregunta {
     private String contenido;
     private ArrayList<FachadaRespuesta> respuestas;
 
-    public FachadaPregunta(int iden, String str,  FachadaRespuesta a, FachadaRespuesta b, FachadaRespuesta c,FachadaRespuesta d){
-        id=iden;
-        contenido=str;
-        respuestas.add(a); respuestas.add(b); respuestas.add(c); respuestas.add(d);
+    public Pregunta crearPregunta(String contenido){
+
+        return new Pregunta(contenido,false);
     }
 
+    public void indicarRespuestas(Pregunta pregunta, ArrayList<Respuesta> respuestas) {
+
+        pregunta.setRespuestas();
+    }
 }
