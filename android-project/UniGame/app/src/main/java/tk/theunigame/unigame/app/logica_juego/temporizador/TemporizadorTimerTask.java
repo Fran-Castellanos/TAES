78456b51298d5b828fe2726e1449e26335539ac0
John <jacv050@gmail.com>
2015-04-25 14:18:28 +0200

    Temporizador con listener solo Model

---
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/logica_juego/temporizador/TemporizadorTimerTask.java
@@ -0,0 +1,88 @@
+package tk.theunigame.unigame.app.logica_juego.temporizador;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import tk.theunigame.unigame.app.presentacion.util.Views.OnTiempoListener;
+
+/**
+ * Created by John on 25/04/2015.
+ */
+public class TemporizadorTimerTask {
+    private Handler handler;
+    private Timer timer;
+    private TimerTask timerTask;
+    private int tiempo;
+    private OnTiempoListener listener;
+
+    public TemporizadorTimerTask(){
+        tiempo = 60;
+        listener = null;
+        initHandler();
+    }
+
+    //Handler para comunicarnos desde el hilo al Textiew
+    private void initHandler(){
+
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                tiempo--;
+            }
+        };
+
+    }
+
+    //Detiene la cuenta
+    public void Parar() {
+        if(timer!=null) {
+            timer.cancel();
+            timer=null;
+        }
+        if(listener != null)
+            listener.onParar(this);
+    }
+
+    //Continua la cuenta
+    public void Continuar() {
+        if(timer==null) {
+            timer = new Timer();
+
+            //Iniciamos el timer task
+            timerTask= new TimerTask() {
+                @Override
+                public void run() {
+                    handler.sendEmptyMessage(0);
+                }
+            };
+
+            //Programamos la tarea para que se ejecute una vez por segundo, es decir decremente en uno
+            //el valor de la vista
+            timer.schedule(timerTask, 0, 1000);
+        }
+        if(listener != null)
+            listener.onContinuar(this);
+    }
+
+    //Reinicia la cuenta
+    public void Reiniciar() {
+        tiempo = 60;
+        if(listener != null)
+            listener.onReiniciar(this);
+    }
+
+    //Introduce un listner en el objeto
+    public void setOnTiempoListener(OnTiempoListener listener){
+        this.listener = listener;
+    }
+
+    //Devuelve el valor del tiempo
+    public int getTiempo(){
+        return tiempo;
+    }
+}
--- android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/presentacion/util/Views/OnTiempoListener.java
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/presentacion/util/Views/OnTiempoListener.java
@@ -2,13 +2,15 @@ package tk.theunigame.unigame.app.presentacion.util.Views;
 
 import java.util.EventListener;
 
+import tk.theunigame.unigame.app.logica_juego.temporizador.TemporizadorTimerTask;
+
 /**
  * Created by John on 21/04/2015.
  */
 public interface OnTiempoListener extends EventListener {
-    public void onParar();
-    public void onContinuar();
-    public void onReiniciar();
+    public void onParar(TemporizadorTimerTask object);
+    public void onContinuar(TemporizadorTimerTask object);
+    public void onReiniciar(TemporizadorTimerTask object);
 
     public void onReset(int tiempo);
     public void onContinue(int tiempo);
