f8a6605185bc95cf3df49a2232b7be267cf7cb17
fenix <felixem@fenix>
2015-05-02 15:48:30 +0200

    Seleccionada la selección aleatoria de preguntas

--- android-project/UniGame/app1/src/main/java/juego/taes/domainmodel/Repository/PreguntaRepository.java
+++ android-project/UniGame/app1/src/main/java/juego/taes/domainmodel/Repository/PreguntaRepository.java
@@ -107,8 +107,10 @@ public class PreguntaRepository {
         try {
 
             QueryBuilder<Pregunta,Integer> builder = dao.queryBuilder();
-            builder.selectRaw("select * from "+ Pregunta.TABLA +" where " + Pregunta.BD + " = " + bolsaId + " ORDER BY RAND() LIMIT 1");
-            return builder.queryForFirst();
+            builder.where().eq(Pregunta.BD, bolsaId);
+            builder.orderByRaw("RANDOM()");
+            Pregunta preg = builder.queryForFirst();
+            return preg;
 
         } catch (SQLException e) {
             //TODO GESTION DE ERRORES
