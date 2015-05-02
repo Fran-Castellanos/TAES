b45224ef09a936881390da7e2c4294dc80bb98c0
Fran-Castellanos <fjcr88@gmail.com>
2015-05-02 12:44:37 +0200

    Pantalla de carga

---
+++ android-project/UniGame/app/src/main/java/tk/theunigame/unigame/app/presentacion/controlador/impl/Cargar.java
@@ -0,0 +1,26 @@
+package tk.theunigame.unigame.app.presentacion.controlador.impl;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import tk.theunigame.unigame.R;
+
+/**
+ * Created by Paco on 02/05/2015.
+ */
+public class Cargar extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargar);
+
+
+        ProgressDialog dialog = ProgressDialog.show(Cargar.this, "",
+                "Cargando...", true);
+    }
+
+
+}
